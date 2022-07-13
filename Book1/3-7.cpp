4a96251c047fbf9ad4b0284350930f930eae7976
JackLee404 <95456297+JackLee404@users.noreply.github.com>
2022-07-13 21:47:11 +0800

    Create 3-7.cpp

---
+++ Book1/3-7.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cctype>
+#include <cstring>
+using namespace std;
+
+const char* rev = "A   3  HIL JM O   2TUVWXY51SE Z  8";
+const char* msg[] = {"is not a palindrom", "is a regular palindrom", "is a mirrored string", "is a mirrored palindrome"};
+
+char r(char ch){
+	if (isalpha(ch)) return rev[ch - 'A'];
+	else return rev[25 + ch - '0'];
+}
+
+int main(){
+	char ch[30];
+	while (scanf("%s", ch) == 1){
+		bool con1 = true, con2 = true;
+		for (int i = 0, j = strlen(ch) - 1; i < j; i ++, j --){
+			if (ch[i] != ch[j]) con1 = false;
+			if (r(ch[j]) != ch[i]) con2 = false;
+		}
+		if (con1 && con2) puts(msg[3]);
+		else if(!con1 && !con2) puts(msg[0]);
+		else if(!con1) puts(msg[2]);
+		else puts(msg[1]);
+	}
+}
