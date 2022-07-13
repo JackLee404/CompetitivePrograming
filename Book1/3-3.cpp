13fa8be26565447a4a14c913c96ab108fc4c68e0
JackLee404 <95456297+JackLee404@users.noreply.github.com>
2022-07-13 17:44:07 +0800

    Create 3-3.cpp

---
+++ Book1/3-3.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <cstring>
+#include <cstdio>
+#define LOCAL
+using namespace std;
+
+int main(){
+	int n, a[500][500];
+	memset(a, 0, sizeof(a));
+	cin >> n;
+	int x = 0, y = n, tot = 1;
+	while (tot < n * n + 1){
+		while (x < n && !a[x + 1][y]) a[++ x][y] = tot ++;
+		while (y > 1 && !a[x][y - 1]) a[x][-- y] = tot ++;
+		while (x > 1 && !a[x - 1][y]) a[-- x][y] = tot ++; 
+		while (y < n - 1 && !a[x][y + 1]) a[x][++ y] = tot ++;
+	} 
+	for (int i = 1; i <= n; i ++){
+		for (int j = 1; j <= n; j ++)
+			printf("%3d", a[i][j]);
+		cout << "\n";
+	}
+	
+}
